38136c0c5eaefbcda1f8705bef823f6e07725581
steflewandowski <stef@stef.io>
2010-12-13 10:23:20 +0000

    give a bit of instruction. Get it online at heroku

--- app.rb
+++ app.rb
@@ -2,11 +2,32 @@ require 'rubygems'
 require 'sinatra'
 require 'mofo'
 require "sinatra/jsonp"
+require "json"
+
+class Microformat
+  def as_json(opts={})
+    self.to_json(opts)
+  end
+  
+  def to_json(opts={})
+    puts "To json"
+    r = {}
+    if self.respond_to?(:properties)
+      self.properties.each do |p|
+        r[p.to_sym] = self.send(p.to_sym)
+      end
+    else
+      r = self.to_s
+    end
+    {self.class.to_s.to_sym => r}.to_json
+  end
+end
 
 get '/' do
   haml :home
 end
 
 get '/parse' do
+  content_type :json
   jsonp Microformat.find params[:url]
 end
